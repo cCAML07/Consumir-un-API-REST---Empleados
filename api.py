e531f9e95efeaf9845d6d91f7196de5948f351fe
PC ryzen 3 <moriracristian07@gmail.com>
2023-08-01 16:47:23 -0500

    nombre

---
+++ api.py
@@ -0,0 +1,51 @@
+import requests
+
+
+url = 'https://dummy.restapiexample.com/api/v1/employees'
+
+
+headers = requests.utils.default_headers()
+headers.update({
+    'User-Agent': 'My User Agent 1.0',
+})
+
+
+response = requests.get(url, headers=headers)
+
+
+if response.status_code == 200:
+    data = response.json()
+
+    #cantidad de empleados
+    total_employees = len(data)
+
+    #promedio de salario de los empleados
+    total_salary = sum(float(employee['employee_salary']) for employee in data['data'])
+    average_salary = total_salary / total_employees
+
+    #promedio de edad de los empleados
+    total_age = sum(int(employee['employee_age']) for employee in data['data'])
+    average_age = total_age / total_employees
+
+    #salario mínimo y máximo
+    salaries = [float(employee['employee_salary']) for employee in data['data']]
+    min_salary = min(salaries)
+    max_salary = max(salaries)
+
+    # edad mínima y máxima
+    ages = [int(employee['employee_age']) for employee in data['data']]
+    min_age = min(ages)
+    max_age = max(ages)
+
+    # Imprimir los resultados
+    print(f"Número de empleados: {total_employees}")
+    print(f"Promedio de salario: {average_salary}")
+    print(f"Promedio de edad: {average_age}")
+    print(f"Salario mínimo: {min_salary}")
+    print(f"Salario máximo: {max_salary}")
+    print(f"Edad menor: {min_age}")
+    print(f"Edad mayor: {max_age}")
+
+else:
+    print("Error al obtener los datos de empleados. Código de estado:", response.status_code)
+
